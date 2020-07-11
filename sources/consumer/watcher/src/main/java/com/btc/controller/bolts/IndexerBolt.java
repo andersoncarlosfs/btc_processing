cfd766406c1c9a28d540b48cde8fcc1b6d4ab4d2
Anderson Carlos Ferreira da Silva <andersoncarlosfs@outlook.com>
2020-07-12 01:33:31 +0200

    finally working

--- sources/consumer/watcher/src/main/java/com/btc/controller/bolts/IndexerBolt.java
+++ sources/consumer/watcher/src/main/java/com/btc/controller/bolts/IndexerBolt.java
@@ -40,6 +40,6 @@ public class IndexerBolt extends BaseIndexerElasticSearchBolt {
 
     @Override
     public void prepare(Map<String, Object> topoConf, TopologyContext context, OutputCollector collector) {
-   }
-    
+    }
+
 }
